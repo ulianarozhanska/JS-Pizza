e5552a5acbcc57c052f293b9b9dc88f83608b273
Uliana <ulianarozhanska@gmail.com>
2018-05-02 00:19:36 +0300

    HW4

--- Frontend/src/Templates.js
+++ Frontend/src/Templates.js
@@ -1,6 +1,7 @@
 /**
  * Created by chaika on 02.02.16.
  */
+
 var fs = require('fs');
 var ejs = require('ejs');
 
@@ -8,3 +9,4 @@ var ejs = require('ejs');
 exports.PizzaMenu_OneItem = ejs.compile(fs.readFileSync('./Frontend/templates/PizzaMenu_OneItem.ejs', "utf8"));
 
 exports.PizzaCart_OneItem = ejs.compile(fs.readFileSync('./Frontend/templates/PizzaCart_OneItem.ejs', "utf8"));
+
